bc8956cb7c4aa301a57970acfe4d813907d02a58
root <root@kali.alameen>
2024-03-30 08:46:26 -0400

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This script fetches https://alx-intranet.hbtn.io/status using urllib
+and displays the body of the response with proper tabulation.
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("- body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body.decode('utf-8'))
